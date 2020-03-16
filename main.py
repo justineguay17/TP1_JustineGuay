c022fe60d493ccf3c2e7f1f074ebda9a21b02353
Justine Guay <62118860+justineguay17@users.noreply.github.com>
2020-03-15 20:52:35 -0400

    Ajout analyser_commande

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import argparse
 
+
+def analyser_commande():
+    parser = argparse.ArgumentParser(description='Jeu Quoridor - phase 1')
+    parser.add_argument('IDUL', metavar='IDUL',
+                        default='JUGUA17', help="IDUL du joueur")
+    parser.add_argument('-l', '--lister', dest='liste',
+                        help='Lister les identifiants de vos 20 dernières parties.')
+    return parser.parse_args()
