d8ae204b43db5a2ee51aca7edfddc919f0b82a7b
Justine Guay <62118860+justineguay17@users.noreply.github.com>
2020-03-16 06:45:47 -0400

    Ajout initialiser_partie

--- api.py
+++ api.py
@@ -14,3 +14,19 @@ def lister_parties(idul):
         print("Le GET sur '{}' a produit le code d'erreur {}.".format(
             url_lister, response.status_code)
         )
+
+        
+def initialiser_partie(idul):
+    url_init = 'https://python.gel.ulaval.ca/quoridor/api/initialiser/'
+    try:
+        response = requests.post(url_init, data={'idul': idul})
+        if response.status_code == 200:
+            json_res = response.json()
+            return json_res['id'], json_res['état']
+        else:
+            print("Le POST sur '{}' a produit le code d'erreur {}.".format(
+                url_init, response.status_code)
+            )
+    except RuntimeError as error:
+        print(error)
+        
