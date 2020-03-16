ffdeb810d4b6ca7e079599aed5ecb913adf04489
Justine Guay <62118860+justineguay17@users.noreply.github.com>
2020-03-15 20:53:55 -0400

    Ajout lister_parties

--- api.py
+++ api.py
@@ -1,1 +1,16 @@
+import requests
+import json
 
+
+def lister_parties(idul):
+    url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
+    response = requests.get(url_lister, params={'idul': idul})
+    if response.status_code == 200:
+        response = response.text
+        json_var = json.loads(response)
+        json_str = json.dumps(json_var, indent=2)
+        print(json_str)    
+    else:
+        print("Le GET sur '{}' a produit le code d'erreur {}.".format(
+            url_lister, response.status_code)
+        )
