8d616ee775ef4c7c2c42a30cd0fc27395d697b9d
Justine Guay <62118860+justineguay17@users.noreply.github.com>
2020-03-18 18:34:24 -0400

    Certains ajustement et ajout de commentaires.
    Traitement de 'message' dans la fonction jouer_coup().

--- api.py
+++ api.py
@@ -3,6 +3,12 @@ import json
 
 
 def lister_parties(idul):
+    """
+    Permet d'obtenir la liste des dernières parties démarrées ou terminées par le joueur de l'idul.
+    Les parties sont listées à partir du serveur du cour.
+    :param idul: Chaine de caractère associée au dossier du joueur.
+    :return: Ne retourne rien. Affiche à la console un dictionnaire json.
+    """
     url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
     response = requests.get(url_lister, params={'idul': idul})
     if response.status_code == 200:
@@ -12,11 +18,16 @@ def lister_parties(idul):
         print(json_str)    
     else:
         print("Le GET sur '{}' a produit le code d'erreur {}.".format(
-            url_lister, response.status_code)
-        )
+            url_lister, response.status_code
+        ))
 
-        
+
 def initialiser_partie(idul):
+    """
+    Permet de démarrer une partie contre le robot du serveur du cour.
+    :param idul: Chaine de caractère associée au dossier du joueur.
+    :return: Retourne le tuple ( identification de partie , état de jeu )
+    """
     url_init = 'https://python.gel.ulaval.ca/quoridor/api/initialiser/'
     try:
         response = requests.post(url_init, data={'idul': idul})
@@ -25,20 +36,31 @@ def initialiser_partie(idul):
             return json_res['id'], json_res['état']
         else:
             print("Le POST sur '{}' a produit le code d'erreur {}.".format(
-                url_init, response.status_code)
-            )
+                url_init, response.status_code
+            ))
     except RuntimeError as error:
         print(error)
 
-        
+
 def jouer_coup(id_partie, type_coup, position):
+    """
+    Permet de jouer un coup contre le robot du serveur du cour.
+    :param id_partie: La chaine de caractère identifiant la partie.
+    :param type_coup: 'D' pour déplacement, 'MH' pour mur horizontal et pout 'MV' mur vertical.
+    :param position: Un tuple ( x, y ) pour positionner un déplacement ou un mur.
+    :return: Si sans erreur, un dictionnaire json d'état de jeu.
+    """
     url_coup = 'https://python.gel.ulaval.ca/quoridor/api/jouer/'
     try:
-        response = requests.post(url_coup, data={'id': id_partie, 'type': type_coup, 'pos': position})
+        response = requests.post(url_coup, data={
+            'id': id_partie, 'type': type_coup, 'pos': position
+        })
         if response.status_code == 200:
             json_res = response.json()
             if 'gagnant' in json_res:
                 raise StopIteration(json_res['gagnant'])
+            elif 'message' in json_res:
+                print(json_res['message'])
             else:
                 return json_res
         else:
@@ -47,4 +69,4 @@ def jouer_coup(id_partie, type_coup, position):
             )
     except RuntimeError as error:
         print(error)
-        
+
