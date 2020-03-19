73c14c1a18323110fd437a639d16ec0a19f85c47
Justine Guay <62118860+justineguay17@users.noreply.github.com>
2020-03-18 20:19:48 -0400

    Modification lister_partie
    try:
    except

--- api.py
+++ api.py
@@ -10,17 +10,20 @@ def lister_parties(idul):
     :return: Ne retourne rien. Affiche à la console un dictionnaire json.
     """
     url_lister = 'https://python.gel.ulaval.ca/quoridor/api/lister/'
-    response = requests.get(url_lister, params={'idul': idul})
-    if response.status_code == 200:
-        response = response.text
-        json_var = json.loads(response)
-        json_str = json.dumps(json_var, indent=2)
-        print(json_str)    
-    else:
-        print("Le GET sur '{}' a produit le code d'erreur {}.".format(
-            url_lister, response.status_code
-        ))
-
+    try:
+        response = requests.get(url_lister, params={'idul': idul})
+        if response.status_code == 200:
+            response = response.text
+            json_var = json.loads(response)
+            json_str = json.dumps(json_var, indent=2)
+            print(json_str)    
+        else:
+            print("Le GET sur '{}' a produit le code d'erreur {}.".format(
+                url_lister, response.status_code
+            ))
+    except RuntimeError as error:
+        print(error)
+            
 
 def initialiser_partie(idul):
     """
